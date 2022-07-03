3cf0c70ce2b9b033ec35e6d20ed56596ba9462b4
IgnacioMelo <ignaciomelo25@gmail.com>
2022-07-03 18:58:39 -0300

    4ta commit ProyectoCoder

---
+++ AppCoder/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('curso/',curso),
+    path('cursos/',cursos),
+    path('estudiantes/',estudiantes),
+    path('profesores/',profesores),
+    path('entregables/',entregables),
+    path('', inicio),
+
+]
+
